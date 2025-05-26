55f9ba441c622b551eb4a90cffb58f0fd67a03b8
Arthur Malakhau <arthur.12345.mal@gmail.com>
2025-05-26 17:40:09 +0300

    hi erel

--- get_data.py
+++ get_data.py
@@ -3,10 +3,10 @@ import urllib.request
 import urllib.parse
 import pandas as pd
 import json
-import os
+import ssl
 import ast
 
-ENVELOPE_PATH = r"C:\Users\erelb\OneDrive\Desktop\השלמה\האקתון\data\gaza_envelope.txt"
+ENVELOPE_PATH = r"data/gaza_envelope.txt"
 def pop_func(db, gaza_envelope, *args):
     file = open(gaza_envelope, "r", encoding = "utf-8")
     envelope_string = file.read()
@@ -21,8 +21,9 @@ def bus_func(db, *args):
 
 def train_func(db, *args):
     return db
+
 def main():
-    os.chdir("data")
+    context = ssl._create_unverified_context()
     RESOURCE_IDS = {"population": ("64edd0ee-3d5d-43ce-8562-c336c24dbc1f", pop_func), "bus": ("86eceab6-44ac-4301-a6a7-9a4a92dae48b", bus_func), "train": ("6cf35ec2-c0eb-4ef0-a904-f093dab0abfd", train_func)}
     for key in RESOURCE_IDS:
         url = 'https://data.gov.il/api/3/action/datastore_search?resource_id=' + RESOURCE_IDS[key][0]
@@ -36,11 +37,11 @@ def main():
             parsed_url.fragment
         ))
         # Now open the encoded URL
-        fileobj = urllib.request.urlopen(encoded_url)
+        fileobj = urllib.request.urlopen(encoded_url, context=context)
         json_val = json.loads(fileobj.read().decode('utf-8'))["result"]["records"]
         df = pd.DataFrame(json_val)
         df = RESOURCE_IDS[key][1](df, ENVELOPE_PATH)
-        df.to_csv(key + "_data.csv", index=False, encoding="utf-8-sig")
+        df.to_csv("data/" + key + "_data.csv", index=False, encoding="utf-8-sig")
         
 if __name__ == "__main__":
     main()
