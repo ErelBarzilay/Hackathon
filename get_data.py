93d80e28fdefb2d512b6b1de4e95ef7b02456ab0
ErelBarzilay <erelbarzilay@mail.tau.ac.il>
2025-05-26 19:41:48 +0300

    i hate this

--- get_data.py
+++ get_data.py
@@ -7,6 +7,8 @@ import ssl
 import ast
 
 ENVELOPE_PATH = r"data/gaza_envelope.txt"
+BUS_PATH = r"data/bus_envelope.txt"
+TRAIN_PATH = r"."
 def pop_func(db, gaza_envelope, *args):
     file = open(gaza_envelope, "r", encoding = "utf-8")
     envelope_string = file.read()
@@ -16,15 +18,19 @@ def pop_func(db, gaza_envelope, *args):
     db = db[db["שם_ישוב"].isin(hebrew_list)]
     return db
     
-def bus_func(db, *args):
+def bus_func(db, envelope, *args):
+    file = open(envelope, "r", encoding = "utf-8")
+    envelope_string = file.read()
+    hebrew_list = ast.literal_eval(envelope_string)
+    db = db[db["cluster_nm"].isin(hebrew_list)]
     return db
 
-def train_func(db, *args):
+def train_func(db, gaza_envelope ,*args):
     return db
 
 def main():
     context = ssl._create_unverified_context()
-    RESOURCE_IDS = {"population": ("64edd0ee-3d5d-43ce-8562-c336c24dbc1f", pop_func), "bus": ("86eceab6-44ac-4301-a6a7-9a4a92dae48b", bus_func), "train": ("6cf35ec2-c0eb-4ef0-a904-f093dab0abfd", train_func)}
+    RESOURCE_IDS = {"population": ("64edd0ee-3d5d-43ce-8562-c336c24dbc1f", pop_func, ENVELOPE_PATH), "bus": ("86eceab6-44ac-4301-a6a7-9a4a92dae48b", bus_func, BUS_PATH), "train": ("6cf35ec2-c0eb-4ef0-a904-f093dab0abfd&q=שדרות", train_func, TRAIN_PATH)}
     for key in RESOURCE_IDS:
         url = 'https://data.gov.il/api/3/action/datastore_search?resource_id=' + RESOURCE_IDS[key][0]
         parsed_url = urllib.parse.urlsplit(url)
@@ -40,7 +46,7 @@ def main():
         fileobj = urllib.request.urlopen(encoded_url, context=context)
         json_val = json.loads(fileobj.read().decode('utf-8'))["result"]["records"]
         df = pd.DataFrame(json_val)
-        df = RESOURCE_IDS[key][1](df, ENVELOPE_PATH)
+        df = RESOURCE_IDS[key][1](df, RESOURCE_IDS[key][2])
         df.to_csv("data/" + key + "_data.csv", index=False, encoding="utf-8-sig")
         
 if __name__ == "__main__":
